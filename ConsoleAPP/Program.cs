4795157b633c327ff636df51c178b51cbfd55d41
myname <you@example.com>
2016-03-21 20:35:02 +0800

    520553

--- ConsoleAPP/Program.cs
+++ ConsoleAPP/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleAPP
             Console.Out.WriteLine("88888888888");
             Console.Out.WriteLine("9999999999999");
             Console.Out.WriteLine("765476547654");
+            Console.Out.WriteLine("77777777777");
         }
     }
 }
